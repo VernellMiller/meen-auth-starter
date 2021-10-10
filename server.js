18859bec9639c608cb3e9fc21cda4870d79ab20e
vernellmiller <nezeey@gmail.com>
2021-10-10 19:22:00 -0400

    configers connection to mongoDB

--- server.js
+++ server.js
@@ -1,14 +1,66 @@
+// require dependencies
 const express = require("express");
+const mongoose = require("mongoose");
 
+// Initialize express App
 const app = express();
 
+// Configure Settings
 require("dotenv").config();
+const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
 
+// Database Configuration
+mongoose.connect(DATABASE_URL);
+const db = mongoose.connection;
+
+db.on("connected", () => {
+    console.log(`Connected to MongoDB`)
+});
+
+db.on("error", (error) => {
+    console.log(`An Error Occurred with MongoDB ${error.message}`)
+});
+
+
+//////// ROUTES INDUCES////////
+
+
+// Index route
 app.get("/", (req, res) => {
     res.send("Hello World")
 });
 
-const PORT = process.env.PORT
+
+// New Route
+
+
+
+// Delete Route
+
+
+
+
+// Update Route
+
+
+
+
+// Create Route
+
+
+
+
+// Edit Route
+
+
+
+
+
+// Show Route
+
+
+
 
 
 app.listen(PORT, () => {
